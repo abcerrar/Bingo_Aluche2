df9d51d84b56cf0e3d0f85cee7712d987409dd09
david <davidcoleraplana@gmail.com>
2023-05-18 20:48:28 +0200

    firebase implementado

--- app/src/main/java/com/example/bingo_aluche/MainActivity.java
+++ app/src/main/java/com/example/bingo_aluche/MainActivity.java
@@ -3,12 +3,27 @@ package com.example.bingo_aluche;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class MainActivity extends AppCompatActivity {
+
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        db.collection("users").document("test").get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+            @Override
+            public void onSuccess(DocumentSnapshot documentSnapshot) {
+                Toast.makeText(MainActivity.this, "Hola chicos: " + documentSnapshot.getData().get("nombre"), Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 }
